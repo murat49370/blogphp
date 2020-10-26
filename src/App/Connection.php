a602033dfaa39fc4baaaf608b061ee79d68e25ec
Murat49370 <murat49370@gmail.com>
2020-10-26 09:17:12 +0100

    delete .gitignore file on directory /App

---
+++ src/App/Connection.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App;
+
+
+use PDO;
+
+class Connection
+{
+
+    public static function get_pdo(): PDO
+    {
+
+        return new PDO(DB_DSN, DB_USER, DB_PASSWORD, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
+
+
+
+}
